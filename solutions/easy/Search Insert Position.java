9e32caeeb27eef69dd51bbc570ad1a4f98195cc3
Apoorv <apoorvactor@gmail.com>
2026-09-08 22:13:50 +0530

    Add solution for Search Insert Position

---
+++ solutions/easy/Search Insert Position.java
@@ -0,0 +1,16 @@
+// Title: Search Insert Position
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/search-insert-position/
+
+                high=mid-1;
+            }
+            else{
+                return mid;
+            else if(nums[mid]>target){
+            }
+            }
+        }
+        return low;
+    }
+}
