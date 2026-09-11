bc2ae133bfe6c042f13c942c96ed02d47d7704c6
Apoorv <apoorvactor@gmail.com>
2026-09-11 21:31:53 +0530

    Add solution for Unique 3-Digit Even Numbers

---
+++ solutions/easy/Unique 3-Digit Even Numbers.java
@@ -0,0 +1,26 @@
+// Title: Unique 3-Digit Even Numbers
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/unique-3-digit-even-numbers/
+
+class Solution {
+    public int totalNumbers(int[] digits) {
+        int[] f = new int[10];
+        int res = 0;
+
+        for (int d : digits) f[d]++;
+
+        for (int i = 1; i < 10; i++) 
+            for (int j = 0; j < 10; j++) 
+                for (int k = 0; k < 9; k += 2) {
+                    f[i]--; f[j]--; f[k]--;
+
+                    if (f[i] >= 0 && f[j] >= 0 && f[k] >= 0) res++;
+
+                    f[i]++; f[j]++; f[k]++;
+                }           
+        
+
+        return res;
+    }
+}
